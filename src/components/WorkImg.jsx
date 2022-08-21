df3cfccb9f60d1b9f704895a5239655bda673399
Pankaj Gaikwad <gaikwadpankaj62@gmail.com>
2022-08-21 14:01:19 -0700

    fixes

--- src/components/WorkImg.jsx
+++ src/components/WorkImg.jsx
@@ -7,10 +7,10 @@ const WorkImg = ({ image, url, text }) => {
       className='mx-auto shadow-lg shadow-[#040c16] h-[160px] group rounded-md container flex justify-center content-div items-center font-[Raleway]'
     >
       <div className='opacity-0 group-hover:opacity-100'>
-        <span className='text-2xl font-bold text-white tracking-wider'>
+        {/* <span className='text-2xl font-bold text-white tracking-wider text-center'>
           {text}
-        </span>
-        <div className='pt-8 text-center'>
+        </span> */}
+        <div className='pt-8 text-center mb-4'>
           <a href={url} target={'_blank'}>
             <button className='text-center text-lg px-4 py-3 m-2 font-bold text-gray-700 rounded-lg bg-white'>
               Demo
